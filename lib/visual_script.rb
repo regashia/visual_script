af37cfb42b6ad5d8d5391787aee5b37a5fa1ef36
Regashia <regashia@gmail.com>
2018-08-03 19:15:59 +0900

    [add] hello world

--- lib/visual_script.rb
+++ lib/visual_script.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require "visual_script/version"
+require "sinatra"
+require "sinatra/reloader" if development?
+require "visual_script/app.rb"
 
 module VisualScript
-  # Your code goes here...
+  App.run!
 end
---
+++ lib/visual_script/app.rb
@@ -0,0 +1,14 @@
+module VisualScript
+  class App < Sinatra::Application
+    def slim(view_name, render_application = true)
+      @view_name = view_name
+      super render_application ? :application : @view_name
+    end
+
+    set :views, File.expand_path('../views', __FILE__)
+
+    get "/" do
+      slim :home
+    end
+  end
+end
